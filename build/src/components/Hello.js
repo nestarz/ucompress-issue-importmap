201f03168a6fec68087f8e6c34edc4264730d6a3
Elias Rhouzlane <eliasrhouzlane@Eliass-MacBook-Pro.local>
2020-05-22 18:36:03 +0200

    add asset

--- build/src/components/Hello.js
+++ build/src/components/Hello.js
@@ -1,2 +1,4 @@
 import React from "react";
-export default ({children}) => React.createElement("div", null, "Hello ", children);
+export default ({children}) => React.createElement("div", null, "Hello ", children, " ", React.createElement("div", null, React.createElement("img", {
+  src: "assets/finger.png"
+})));
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,3 +1,10 @@
 import React from "react";
 
-export default ({ children }) => <div>Hello {children}</div>;
+export default ({ children }) => (
+  <div>
+    Hello {children}{" "}
+    <div>
+      <img src="assets/finger.png" />
+    </div>
+  </div>
+);
