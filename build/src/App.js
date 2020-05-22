a6b8b4aaca207c9db00aa191c16e3e41e60b40ab
Elias Rhouzlane <eliasrhouzlane@Eliass-MacBook-Pro.local>
2020-05-22 18:25:45 +0200

    first commit

---
+++ build/src/App.js
@@ -0,0 +1,3 @@
+import React from "react";
+import Hello2 from "./../../src/components/Hello.jsx";
+export default () => React.createElement(Hello2, null, "World");
---
+++ src/App.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import Hello from "./components/Hello.jsx";
+
+export default () => <Hello>World</Hello>;
